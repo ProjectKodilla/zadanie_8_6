88e4e8cf8d1829bfa59cc886e278dc8109c7dcd5
Marcin Bogdanski <marcin@bogdanski.info>
2018-02-19 16:26:59 +0100

    init start

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// scripts.js
+// komentrza jednoliniowy
+/* komentarz blokowy
+do pokazania, że można
+blokować więcej linii */
+
+
+var name = prompt('Enter your name');
+// komentrz do lini alert wg zadania alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+// koniec wcześniejszych zadań
+
+
+// zadanie_8_5
+
+var a = 6
+var h = 3
+var triangleArea = a*h/2
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+alert('Pole trójkąta wynosi ' + triangleArea)
+
+//koniec zadania_8_5
